57ae2d4c23440a7e60367d6850dbcac8c886dbf8
Your Name <liangfan502@sina.com>
2020-06-04 15:51:38 +0800

    test

---
+++ test-lib/src/main/java/com/bq/test_lib/TestLib.java
@@ -0,0 +1,14 @@
+package com.bq.test_lib;
+
+/**
+ * 文件名：
+ * 描述：
+ * 作者：梁帆
+ * 时间：2020/6/4
+ * 版权：
+ */
+public class TestLib {
+    public void fun(){
+        System.out.println("===============================fun");
+    }
+}
