b16c4f44309a3ed82e95decf2eba04d0c2e28c89
Your Name <liangfan502@sina.com>
2020-06-04 16:09:25 +0800

    yoyo1

---
+++ lib2/src/main/java/com/bq/lib2/LibTest2.java
@@ -0,0 +1,15 @@
+package com.bq.lib2;
+
+/**
+ * 文件名：
+ * 描述：
+ * 作者：梁帆
+ * 时间：2020/6/4
+ * 版权：
+ */
+public class LibTest2 {
+
+    public void fun2(){
+        System.out.println("yoyo-----------------");
+    }
+}
